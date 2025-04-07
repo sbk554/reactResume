00fa01c84ec2c5e5166c66de71585088decc1005
sbk554 <sbk8731@naver.com>
2025-04-07 10:36:05 +0900

    zzzz

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -23,6 +23,68 @@ export default function Project(){
                     <li></li>
                 </ul>
             </div>
+
+            <div className="project-content">
+                <span>Mmate 플래시21 솔루션</span>
+                <div className="project-date">
+                    <p>(주)플래시21</p>
+                    <p>2022.11.01 - 2024.10.24</p>
+                </div>
+                <p>
+                    <a href="https://mmate.flash21.com" target="_blank">https://mmate.flash21.com</a>
+                </p>
+                <p>인원 : 2명(프론트 1명 ,백엔드 1명)</p>
+                <p>유지보수 기간 : 2년</p>
+                <p>추가개발 : 6개월</p>
+                <p>[담당한부분]</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div className="project-content">
+                <span>경기도국민안전체험관</span>
+                <div className="project-date">
+                    <p>(주)플래시21</p>
+                    <p>2023.01.02 - 2023.03.30</p>
+                </div>
+                <p>
+                    <a href="https://ggsec.gg.go.kr/index" target="_blank">https://ggsec.gg.go.kr/index</a>
+                </p>
+                <p>인원 : 7명(프론트 3명 ,백엔드 4명)</p>
+                <p>개발기간 : 3개월</p>
+                <p>[담당한부분]</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div className="project-content">
+                <span>대구광역시건축사회 건축사 업무지원시스템</span>
+                <div className="project-date">
+                    <p>(주)플래시21</p>
+                    <p>	2023.07.07 - 2023.10.20</p>
+                </div>
+                <p>
+                    <a href="https://kirasys.kr/" target="_blank">https://kirasys.kr/</a>
+                </p>
+                <p>인원 : 5명(프론트 1명 ,백엔드 4명)</p>
+                <p>개발기간 : 3개월</p>
+                <p>[담당한부분]</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
         </div>
     );
 }
