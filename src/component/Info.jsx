6b9bbd4b54c7fe391be4404b6b8395dadb5bd091
sbk554 <sbk8731@naver.com>
2025-04-16 11:14:14 +0900

    zzzz

--- src/component/Info.jsx
+++ src/component/Info.jsx
@@ -1,21 +1,40 @@
+import { useEffect, useState } from "react";
 import person from "../assets/person.jpg";
 import "../css/Info.css";
 
 export default function Info(){
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 500);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <div id="info">
-            <div className="info-wrap">
+            <div className="info-wrap" id="info-wrap">
+                {isMobile && (
+                    <div className="info-wrap-img">
+                        <img src={person} alt="person" />
+                    </div>
+                )}
                 <div className="info-wrap-content">
                     <span className="info-wrap-content-name">성보경</span>
                     <span className="info-wrap-content-history">2년차 백엔드 개발자</span>
-                    <div>
+                    <div className="info-wrap-email">
                         <span>이메일</span>
                         <span>sbk8731@naver.com</span>
                     </div>
                 </div>
-                <div className="info-wrap-img">
-                    <img src={person} />
-                </div>
+                {!isMobile && (
+                    <div className="info-wrap-img">
+                        <img src={person} alt="person" />
+                    </div>
+                )}
             </div>
             <div className="info-wrap-main">
                 <span>사용자에게 가치 있는 서비스를 제공하며, 그 안에서 성취감과 애정을 느낄 수 있는 일을 하고 싶어 개발자의 길을 선택하게 되었습니다.</span>
